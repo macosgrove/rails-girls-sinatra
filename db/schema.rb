bfd9d19ffd4b569e834d335b65267b0aba13bf5c
Mary-Anne Cosgrove <mary-anne.cosgrove@envato.com>
2016-02-10 02:07:45 +0000

    Ideas and files

--- db/schema.rb
+++ db/schema.rb
@@ -13,4 +13,12 @@
 
 ActiveRecord::Schema.define(version: 20160210005122) do
 
+  create_table "ideas", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "picture"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
 end
